67b187569f0a046f6f37b68dcac62c36039f7e3d
Ярослав Каніковський <110105516+KanikovckiyYaroslav@users.noreply.github.com>
2022-09-24 16:43:59 +0300

    Create 1.1.py

---
+++ 1.1.py
@@ -0,0 +1,5 @@
+"Напишіть програму, яка запитує три цілі числа a, b і x та друкує їх добуток."
+a = int(input("Number 1: "))
+b = int(input("Number 2: "))
+x = int(input("Number 3: "))
+print(a*b*x)
